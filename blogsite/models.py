338e37d30a7cb3d08d0bd0fac4eac441efe3c31d
Sergey <seri05@yandex.ru>
2018-03-06 18:11:33 +0300

    Изменены названия столбцов страниц

--- blogsite/models.py
+++ blogsite/models.py
@@ -3,19 +3,19 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Blog(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     blog_name = models.CharField(max_length=100)
 
 class Post(models.Model):
     post_header = models.CharField(max_length=100, null=False)
     post_text = models.TextField(null=False)
-    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
 class BlogSubscriber(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
 
 class UserPostWatched(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
