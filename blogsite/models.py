d6b708bbcf3b47d4d64a28546e6e161873115903
Sergey <seri05@yandex.ru>
2018-03-06 18:06:13 +0300

    Созданы модели для blogsite

--- blogsite/models.py
+++ blogsite/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Blog(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog_name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    post_header = models.CharField(max_length=100, null=False)
+    post_text = models.TextField(null=False)
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class BlogSubscriber(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+class UserPostWatched(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
